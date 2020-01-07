77982d2e176cb8e96a5af3e7a08aef1d943e7422
Hung Phan <17309881+hp0101@users.noreply.github.com>
2020-01-06 16:32:33 -0800

    added UI

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var billAmountStackView: UIStackView!
+    @IBOutlet weak var tipPercentageStackView: UIStackView!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func sliderAction(_ sender: UISlider) {
+        // Uncheck "Continuous Updates" for Events
+        sender.setValue(sender.value.rounded(.down), animated: true)
+        
+        let charArr = splitLabel.text?.split(separator: " ")
+        
+        if (Int(sender.value) == 1) {
+            splitLabel.text = "Split For 1 Person"
+        }
+        else {
+            let newText = "\(charArr![0]) \(charArr![1]) \(Int(sender.value)) People"
+            
+            splitLabel.text = newText
+        }
+    }
+    
+    // add underline for each stack view
+    func addUnderline(inputView : UIStackView) {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: inputView.frame.height - 1, width: inputView.frame.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor(red: 138/255.0, green: 242/255.0, blue: 160/255.0, alpha: 1.0).cgColor
+        inputView.layer.addSublayer(bottomLine)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // add underline for each stack view
+        addUnderline(inputView: billAmountStackView)
+        addUnderline(inputView: tipPercentageStackView)
     }
-
 
 }
 
